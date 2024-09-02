f6dcb922b3d5af2440082447e1283204308e369f
ShubhamKumar37 <sk9818281820@gmail.com>
2024-09-03 01:13:20 +0530

    new approach for a question

--- Array/Question_Lvl_2/countPairKDifference.cpp
+++ Array/Question_Lvl_2/countPairKDifference.cpp
@@ -4,7 +4,43 @@
 
 using namespace std; 
 
-int findKDiffPair(vector<int> &arr, int length, int k)
+bool isPossiblePair(vector<int> &arr, int startValue, int s, int e, int k)
+{
+    int mid = s + ((e - s) >> 1);
+
+    while(s <= e)
+    {
+        int diff = abs(arr[mid] - startValue);
+        if(diff == k) return true;
+        else if(diff < k) s = mid + 1;
+        else e = mid - 1;
+
+        mid = s + ((e - s) >> 1);
+    }
+
+    return false;
+}
+
+int findKDiffPair2(vector<int> &arr, int length, int k)
+{
+    int ans = 0;
+    sort(arr.begin(), arr.end());
+
+    for(int i = 0; i < length;)
+    {
+        bool possiblePair = isPossiblePair(arr, arr[i], i + 1, length - 1, k);
+
+        if(possiblePair) ans++;
+
+        while(i + 1 < length && arr[i] == arr[i + 1]) i++;
+
+        i++;
+    }
+
+    return ans;
+}
+
+int findKDiffPair1(vector<int> &arr, int length, int k)
 {
     sort(arr.begin(), arr.end());
 
@@ -18,7 +54,6 @@ int findKDiffPair(vector<int> &arr, int length, int k)
         else if(diff == k)
         {
             ans++;
-            cout << ans << endl;
             int T1 = arr[i++];
             int T2 = arr[j++];
 
@@ -51,7 +86,8 @@ int main()
     }
 
 
-    cout << "Number of pair with K differnce in abs = " << findKDiffPair(arr, arr.size(), k) << endl;
+    // cout << "Number of pair with K differnce in abs = " << findKDiffPair1(arr, arr.size(), k) << endl;
+    cout << "Number of pair with K differnce in abs = " << findKDiffPair2(arr, arr.size(), k) << endl;
     
     return 0;
 }
