7725faf571455f6670d41135c41bea19bf5a055d
ShubhamKumar37 <sk9818281820@gmail.com>
2024-10-13 16:19:36 +0530

    new question solved permutatin with more knowledge

---
+++ Backtracking/Question_Lvl_3/permutation.cpp
@@ -0,0 +1,33 @@
+#include<IOSTREAM>
+#include<STRING>
+
+using namespace std;
+
+void getPermutation(string str, int index)
+{
+    if(index >= str.size())
+    {
+        cout << str << endl;
+    }
+
+    for(int i = index; i < str.size(); i++)
+    {
+        swap(str[i], str[index]);
+        // cout << "index =  " << index << " i = " << i << " Value changed to " << str << endl;
+
+        // Here if we donot backtract using by value call then 
+        // the changed string will going to be passed to recursive call
+        // but the only difference is going to be maked will be the order of the string and nothing else
+        getPermutation(str, index + 1);
+        // swap(str[i], str[index]);
+    }
+}
+
+int main()
+{
+    string str = "abcd";
+
+    getPermutation(str, 0);
+
+    return 0;
+}
