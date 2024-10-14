d6d82257752f7e8c2220770d23bf20a972370b0c
ShubhamKumar37 <sk9818281820@gmail.com>
2024-10-15 01:13:01 +0530

    N-queen problem solved

--- Backtracking/Question_Lvl_3/permutation.cpp
+++ Backtracking/Question_Lvl_3/permutation.cpp
@@ -3,23 +3,39 @@
 
 using namespace std;
 
-void getPermutation(string str, int index)
+// void getPermutation(string str, int index)
+// {
+//     if(index >= str.size())
+//     {
+//         cout << str << endl;
+//     }
+
+//     for(int i = index; i < str.size(); i++)
+//     {
+//         swap(str[i], str[index]);
+//         // cout << "index =  " << index << " i = " << i << " Value changed to " << str << endl;
+
+//         // Here if we donot backtract using by value call then 
+//         // the changed string will going to be passed to recursive call
+//         // but the only difference is going to be maked will be the order of the string and nothing else
+//         getPermutation(str, index + 1);
+//         // swap(str[i], str[index]);
+//     }
+// }
+
+void getPermutation(string &str, int index)
 {
     if(index >= str.size())
     {
         cout << str << endl;
+        return ;
     }
 
     for(int i = index; i < str.size(); i++)
     {
         swap(str[i], str[index]);
-        // cout << "index =  " << index << " i = " << i << " Value changed to " << str << endl;
-
-        // Here if we donot backtract using by value call then 
-        // the changed string will going to be passed to recursive call
-        // but the only difference is going to be maked will be the order of the string and nothing else
         getPermutation(str, index + 1);
-        // swap(str[i], str[index]);
+        swap(str[i], str[index]);
     }
 }
 
