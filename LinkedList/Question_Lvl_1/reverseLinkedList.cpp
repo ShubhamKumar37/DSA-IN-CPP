13623c6c8fba17d5351ccdb3d59afb58f439f0a4
ShubhamKumar37 <sk9818281820@gmail.com>
2024-11-01 17:23:07 +0530

    1 question solved of linked list

---
+++ LinkedList/Question_Lvl_1/reverseLinkedList.cpp
@@ -0,0 +1,28 @@
+ListNode *reverse(ListNode *head, ListNode *prev, ListNode *curr)
+{
+    if (curr == nullptr)
+        return prev;
+
+    ListNode *forward = curr->next;
+    curr->next = prev;
+    return reverse(head, curr, forward);
+}
+
+ListNode *reverseList(ListNode *head)
+{
+    // return reverse(head, nullptr, head);
+    ListNode* curr = head;
+    ListNode* forward = head -> next;
+    ListNode* prev = null;
+
+    while(curr != nullptr)
+    {
+        forward = curr -> next;
+        curr -> next = prev;
+        curr = forward;
+        prev = curr;
+    }
+
+    return prev;
+
+}
--- atCoder.cpp
+++ atCoder.cpp
@@ -1,1 +1,31 @@
-    
+int nums ;
+cin >> nums;
+
+
+
+for(int i = 0; i < nums; i++)
+{
+    int n, k;
+    cin >> n >> k;
+    vector<int> arr;
+    for(int i = 0; i < n; i++)
+    {
+        int ele;
+        cin >> ele;
+        arr.push_back(ele);
+    }
+
+    // Actual code start //
+
+    k = (k % n);
+
+    reverse(arr.begin(), arr.end());
+    reverse(arr.begin(), arr.begin() + k);
+    reverse(arr.begin() + k, arr.end());
+
+    for(auto i : arr) cout << i << " ";
+    cout << endl;
+
+    // Actual code end //
+
+}
