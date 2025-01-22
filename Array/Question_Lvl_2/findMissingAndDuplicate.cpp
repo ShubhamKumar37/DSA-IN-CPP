5265d7d305943435dbff8618ace736edd27ce3a5
ShubhamKumar37 <sk9818281820@gmail.com>
2025-01-22 15:50:31 +0530

    Refactor formatting and spacing in findMissingAndDuplicate.cpp for improved readability

--- Array/Question_Lvl_2/findMissingAndDuplicate.cpp
+++ Array/Question_Lvl_2/findMissingAndDuplicate.cpp
@@ -1,4 +1,4 @@
-#include<IOSTREAM>
+#include <IOSTREAM>
 
 using namespace std;
 
@@ -6,10 +6,10 @@ void findMissingAndDuplicate1(int arr[], int length)
 {
     int duplicateElement = 0;
 
-    for(int i = 0; i < length; i++)
+    for (int i = 0; i < length; i++)
     {
         int index = abs(arr[i]) - 1;
-        if(arr[index] < 0)
+        if (arr[index] < 0)
         {
             duplicateElement = index + 1;
         }
@@ -19,9 +19,9 @@ void findMissingAndDuplicate1(int arr[], int length)
 
     cout << "Duplicate element = " << duplicateElement << endl;
     cout << "Missing Element are :" << endl;
-    for(int i = 0; i < length; i++)
+    for (int i = 0; i < length; i++)
     {
-        if(arr[i] > 0)
+        if (arr[i] > 0)
         {
             cout << i + 1 << endl;
         }
@@ -34,8 +34,6 @@ int main()
     int length = sizeof(arr) >> 2;
 
     findMissingAndDuplicate1(arr, length);
-
-
 
     return 0;
 }
